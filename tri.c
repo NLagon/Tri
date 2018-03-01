d4d0df97544bfd36a9be79fba4e1c3be036263e8
NL <34057013+NLagon@users.noreply.github.com>
2018-03-01 13:26:04 +0100

    Update tri.c

--- tri.c
+++ tri.c
@@ -3,17 +3,18 @@
 
 int main(){
     
-    int tab[]={12,5,2,14};
+    int tab[]={12,5,2,14};  // Déclaration du tableau à trier
     int taille=4;
-    int curseur=0;
+    int curseur=0;          // curseur sur le tableau se plaçant au début de la zone à trier 
+                            
     int min;
     int indice_min=0;
     
     
                 
-       while(curseur<taille){
+       while(curseur<taille){ // Tant que l'on a pas parcouru tout le tableau, le curseur avance et définit une nouvelle zone à trier
 
-           for(int i=curseur;i<taille;i++){
+           for(int i=curseur;i<taille;i++){ // parcours du tableau pour trouver la plus petite valeur
             
                min=tab[curseur];
            
@@ -21,7 +22,7 @@ int main(){
                    //printf("%d",i);
                    min=tab[i];
                    indice_min=i;
-                   int temp=tab[curseur];
+                   int temp=tab[curseur]; // échange de la case contenant la plus petite valeur avec avec la 1 ère case de la zone à trier
                    tab[curseur]=tab[indice_min];
                    tab[indice_min]=temp;
                }
@@ -29,14 +30,14 @@ int main(){
            //printf("%d\n",min);
 
            
-           curseur++;      
+           curseur++; // avancée du curseur      
        }
 
        
     
     for (int i=0;i<taille;i++){
         
-        printf("%d\n",tab[i]);
+        printf("%d\n",tab[i]); // affichage du tableau à trier
     }
 
 }
